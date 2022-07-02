f1dae3fbcae3adb6b381416a9042086e15b0b70f
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-02 17:17:03 +0530

    Add files via upload

---
+++ AltronX/helper_extra/afk_mongo.py
@@ -0,0 +1,20 @@
+from Altron.helper_extra.mongo import db
+
+usersdb = db.users
+
+
+async def is_afk(user_id: int) -> bool:
+    user = usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False, {}
+    return True, user["reason"]
+
+
+async def add_afk(user_id: int, mode):
+    usersdb.update_one({"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True)
+
+
+async def remove_afk(user_id: int):
+    user = usersdb.find_one({"user_id": user_id})
+    if user:
+        return usersdb.delete_one({"user_id": user_id})
