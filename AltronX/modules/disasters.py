868a98e66070652fd76dbea5e22c1594e4bdc7b1
Ash ketchum <102516901+POKELEGEND@users.noreply.github.com>
2022-08-07 22:16:09 +0530

    Update disasters.py

--- AltronX/modules/disasters.py
+++ AltronX/modules/disasters.py
@@ -4,18 +4,18 @@ import json
 import os
 from typing import Optional
 
-from GabiBraunRobot import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
+from AltronX import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
                           TIGERS, WOLVES, dispatcher)
-from GabiBraunRobot.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
+from AltronX.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
                                                            whitelist_plus)
-from GabiBraunRobot.modules.helper_funcs.extraction import extract_user
-from GabiBraunRobot.modules.log_channel import gloggable
+from AltronX.modules.helper_funcs.extraction import extract_user
+from AltronX.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
 ELEVATED_USERS_FILE = os.path.join(os.getcwd(),
-                                   'GabiBraunRobot/elevated_users.json')
+                                   'AltronX/elevated_users.json')
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
