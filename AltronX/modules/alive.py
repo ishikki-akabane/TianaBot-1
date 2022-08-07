9473093a00bc7c13c3c4496d4888e7f1b937dbf0
Ash ketchum <102516901+POKELEGEND@users.noreply.github.com>
2022-08-07 11:42:34 +0530

    Update alive.py

--- AltronX/modules/alive.py
+++ AltronX/modules/alive.py
@@ -8,15 +8,15 @@ from AltronX import telethn as tbot
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [ᴀʟᴛʀᴏɴ ✘ ʀᴏʙᴏᴛ](https://t.me/AltronXRobot)​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝝙𝗟𝗧𝗥𝗢𝗡](https://t.me/TheAltronX)** \n\n"
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [Mɪᴋᴜ Nᴀᴋᴀɴᴏ](https://t.me/MikuX_Robot)​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐿𝑒𝑣𝑖 𝐴𝑐𝑘𝑒𝑟𝑚𝑎𝑛](https://t.me/POKEULTRALEGEND)** \n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telegram.__version__}` \n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{telethon.__version__}` \n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrogram.__version__}` \n━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/AltronXRobot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/TheAltron"),
+            Button.url("ʜᴇʟᴘ​", "https://t.me/MikuX_Robot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/MikuXKurisuSupport"),
         ]
     ]
-    await tbot.send_file(event.chat_id, "https://te.legra.ph/file/1f417d13c7e201d86e91e.jpg", caption=TEXT, buttons=BUTTON)
+    await tbot.send_file(event.chat_id, "https://telegra.ph//file/6a221ea9e540ca49a945f.jpg", caption=TEXT, buttons=BUTTON)
