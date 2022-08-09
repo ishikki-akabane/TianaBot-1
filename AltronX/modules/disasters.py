276eac56dbbc39f1192fae5b0a9d6a02df3d52c9
Ash ketchum <102516901+POKELEGEND@users.noreply.github.com>
2022-08-09 14:08:07 +0530

    Update disasters.py

--- AltronX/modules/disasters.py
+++ AltronX/modules/disasters.py
@@ -540,7 +540,7 @@ def devlist(update: Update, context: CallbackContext):
         "<code>Gathering intel..</code>", parse_mode=ParseMode.HTML
     )
     true_dev = list(set(DEV_USERS) - {OWNER_ID})
-    reply = "<b>Tiana Powerhouse Members ⚡️:</b>\n"
+    reply = "<b>Miku Powerhouse Members ⚡️:</b>\n"
     for each_user in true_dev:
         user_id = int(each_user)
         try:
